8816180a0a60e7158ba93bd65b498a79298a0282
Austin Dryden <austinisnorobot@gmail.com>
2020-01-06 15:36:28 -0500

    adds all but Bonus

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const http = require('http');
 const express = require("express");
+// const fs = require('fs');
+const album = require("./albums");
 const app = express();
 const server = http.createServer(app);
-const PORT = "3000";
+const PORT = "3003";
+// const albumData = fs.readFileSync('./albumsData.json').toString();
+
+app.get('/', (request, response)=>{
+    response.send("go somewhere so I can do something");
+});
+
+app.get('/album/:albumID', (request, response) =>{
+    const albumID = request.params.albumID;
+    response.send(album.getAlbums(albumID));
+
+});
+
+app.get('/album/:albumID/song', (request, response) =>{
+    const albumID = request.params.albumID;
+    response.send(album.getAllSongs(albumID));
+});
+
+app.get('/album/:albumID/song/:songID', (request, response) =>{
+    const albumID = request.params.albumID;
+    const songID = request.params.songID;
+    response.send(album.getSongsForAlbum(albumID,songID));
+});
+
+
+
+
+
+
+
+server.listen(PORT, () =>{
+    console.log(`listening on port ${PORT}`);
+});
